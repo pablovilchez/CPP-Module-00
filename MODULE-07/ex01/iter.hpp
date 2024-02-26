97c049a9cd34fadde078c4a1f5c724751c4faac7
pablovilchez <pablovilchez.r@gmail.com>
2024-02-26 18:36:42 +0100

    Add overloaded iter function. Fix errors

--- MODULE-07/ex01/iter.hpp
+++ MODULE-07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 20:36:16 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/02/25 21:04:34 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/02/26 18:27:27 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 
 
 template <typename T>
-void iter(T *array, int len, T(*func)(T arg))
+void iter(T *array, int len, T(*func)(const T &arg))
 {
 	for (int i = 0; i < len; i++)
 		array[i] = func(array[i]);
+}
+
+template <typename T>
+void iter(T *array, int len, void(*func)(const T &arg))
+{
+	for (int i = 0; i < len; i++)
+		func(array[i]);
 }
 
 #endif
--- MODULE-07/ex01/main.cpp
+++ MODULE-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 20:36:01 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/02/25 21:03:34 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/02/26 18:32:53 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 #include <iostream>
 
 template <typename T>
-T doubleValue(T value) {
+T doubleValue(const T &value) {
     return value * 2;
 }
 
 template <typename T>
-T addOne(T value) {
+T addOne(const T &value) {
     return value + 1;
 }
 
 int main() {
     int intArray[] = {1, 2, 3, 4, 5};
 	
-    ::iter(intArray, 5, doubleValue);
+    iter(intArray, 5, doubleValue);
     for (int i = 0; i < 5; ++i)
         std::cout << intArray[i] << " ";
     std::cout << std::endl;
 
 	float floatArray[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
-	::iter(floatArray, 5, doubleValue);
+	iter(floatArray, 5, doubleValue);
 	for (int i = 0; i < 5; ++i)
 		std::cout << floatArray[i] << " ";
 	std::cout << std::endl;
 
 	char charArray[] = {'a', 'b', 'c', 'd', 'e'};
-	::iter(charArray, 5, addOne);
+	iter(charArray, 5, addOne);
 	for (int i = 0; i < 5; ++i)
 		std::cout << charArray[i] << " ";
 	std::cout << std::endl;
