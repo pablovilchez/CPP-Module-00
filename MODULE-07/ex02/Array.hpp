d08d3c64438c7cd27d2dec0a7319562f2728b5d1
pablovilchez <pablovilchez.r@gmail.com>
2024-02-26 12:51:53 +0100

    some tests

--- MODULE-07/ex02/Array.hpp
+++ MODULE-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 21:06:05 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/02/25 21:59:06 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/02/26 12:10:36 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define ARRAY_HPP
 
 # include <iostream>
-# include "Array.tpp"
 
 template <typename T>
 class Array
@@ -33,10 +32,68 @@ class Array
 		unsigned int size() const;
 
 	private:
-		T *tArray;
-		unsigned int tLen;
+		T *_tArray;
+		unsigned int _tLen;
 
 };
+
+template <typename T>
+Array<T>::Array() : _tArray(NULL), _tLen(0) { }
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _tArray(new T[n]), _tLen(n) { }
+
+template <typename T>
+Array<T>::Array(const Array &other) : _tArray(new T[other._tLen]), _tLen(other._tLen)
+{
+	for (unsigned int i = 0; i < _tLen; i++)
+		_tArray[i] = other._tArray[i];
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	if (_tArray)
+		delete[] _tArray;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+	if (this != &other)
+	{
+		if (_tArray)
+			delete[] _tArray;
+		_tArray = new T[other._tLen];
+		_tLen = other._tLen;
+		for (unsigned int i = 0; i < _tLen; i++)
+			_tArray[i] = other._tArray[i];
+	}
+	return *this;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int i)
+{
+	if (i >= _tLen)
+		throw std::out_of_range("Index out of range");
+	return _tArray[i];
+}
+
+template <typename T>
+const T &Array<T>::operator[](unsigned int i) const
+{
+	if (i >= _tLen)
+		throw std::out_of_range("Index out of range");
+	return _tArray[i];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return _tLen;
+}
+
 
 
 #endif
--- MODULE-07/ex02/main.cpp
+++ MODULE-07/ex02/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <Array.hpp>
 
-#define MAX_VAL 750
+#define MAX_VAL 10
+
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
@@ -13,10 +14,32 @@ int main(int, char**)
         numbers[i] = value;
         mirror[i] = value;
     }
+
+    // show generated arrays:
+    std::cout << "numbers:      ";
+    for (int i = 0; i < MAX_VAL; i++)
+        std::cout << numbers[i] << "  ";
+    std::cout << std::endl;
+    std::cout << "mirror:       ";
+    for (int i = 0; i < MAX_VAL; i++)
+        std::cout << mirror[i] << "  ";
+    std::cout << std::endl;
+    // ++++++++++++++++++++++
+
     //SCOPE
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
+
+        // show generated arrays with copy an equal:
+            std::cout << "(copy) tmp:   ";
+        for (int i = 0; i < MAX_VAL; i++)
+            std::cout << tmp[i] << "  ";
+        std::cout << std::endl;
+            std::cout << "(equal) test: ";
+        for (int i = 0; i < MAX_VAL; i++)
+            std::cout << test[i] << "  ";
+        std::cout << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -27,6 +50,7 @@ int main(int, char**)
             return 1;
         }
     }
+
     try
     {
         numbers[-2] = 0;
@@ -35,6 +59,7 @@ int main(int, char**)
     {
         std::cerr << e.what() << '\n';
     }
+    
     try
     {
         numbers[MAX_VAL] = 0;
@@ -44,10 +69,22 @@ int main(int, char**)
         std::cerr << e.what() << '\n';
     }
 
+    // reasign values to "numbers"
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
     }
+
+    // compare new array with old
+    std::cout << "(new)numbers: ";
+    for (int i = 0; i < MAX_VAL; i++)
+        std::cout << numbers[i] << "  ";
+    std::cout << std::endl;
+    std::cout << "mirror:       ";
+    for (int i = 0; i < MAX_VAL; i++)
+        std::cout << mirror[i] << "  ";
+    std::cout << std::endl;
+
     delete [] mirror;//
     return 0;
 }
