b9859977833127d4753fef795e13bafc3594dae9
pablovilchez <pablovilchez.r@gmail.com>
2024-02-26 10:59:36 +0100

    protect with try-catch

--- MODULE-06/ex01/main.cpp
+++ MODULE-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 11:48:56 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/02/25 16:29:00 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/02/26 09:21:33 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,24 @@
 
 int main()
 {
-	int ptr = 42;
-	Data *stdPtr = new Data(ptr);
-	uintptr_t intPtr = Serializer::serialize(stdPtr);
-	Data *other = Serializer::deserialize(intPtr);
+	try
+	{
+		int ptr = 42;
+		Data *stdPtr = new Data(ptr);
+		uintptr_t intPtr = Serializer::serialize(stdPtr);
+		Data *other = Serializer::deserialize(intPtr);
 
-	std::cout << "Before: " << stdPtr->rawPtr << std::endl;
-	std::cout << "after: " << other->rawPtr << std::endl;
+		std::cout << "Before: " << stdPtr->rawPtr << std::endl;
+		std::cout << "after: " << other->rawPtr << std::endl;
 
-	std::cout << "Before: " << std::hex << stdPtr << std::endl;
-	std::cout << "after: " << std::hex << other << std::endl;
-	
-	delete stdPtr;
+		std::cout << "Before: " << std::hex << stdPtr << std::endl;
+		std::cout << "after: " << std::hex << other << std::endl;
+		
+		delete stdPtr;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
