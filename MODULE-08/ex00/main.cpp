24b572409d72a6808da315b1ddb3a51c1a3e30d2
pablovilchez <pablovilchez.r@gmail.com>
2024-02-28 22:46:14 +0100

    Working. Needs more tests and improve file structure.

--- MODULE-08/ex00/main.cpp
+++ MODULE-08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 10:33:31 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/02/27 14:22:44 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:18:29 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ int main()
 {
 	std::vector<int> vec = {0, 1, 2, 3, 4, 5};
 	easyfind(vec, 3);
+
+	std::vector<char> other = {'a', 'b', 'c', 'd', 'e'};
+	easyfind(other, 'b');
 	
 	return 0;
 }
