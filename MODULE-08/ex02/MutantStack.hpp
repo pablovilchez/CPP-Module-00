f4d3ad215ae8683b5ec808397cf5a3a0f50d8493
Pablo Vílchez <pablovilchez.r@gmail.com>
2024-03-22 00:20:41 +0100

    working on ex02

---
+++ MODULE-08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+
+        ~MutantStack() {}
+
+        MutantStack &operator=(const MutantStack &other)
+        {
+            if (this == &other)
+                return (*this);
+            std::stack<T>::operator=(other);
+            return (*this);
+        }
+
+        typedef typename std::deque<T>::iterator iterator;
+
+        iterator begin() { return (std::stack<T>::c.begin()); }
+        iterator end() { return (std::stack<T>::c.end()); }
+};
+
+#endif
