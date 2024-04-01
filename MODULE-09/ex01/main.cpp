9332314cb5c2d685aa1cfe90ab750a33e1bcb0d3
Pablo Vílchez <pablovilchez.r@gmail.com>
2024-04-01 22:00:20 +0200

    Move function into class

--- MODULE-09/ex01/main.cpp
+++ MODULE-09/ex01/main.cpp
@@ -2,14 +2,13 @@
 
 int main(int argc, char const *argv[]) {
 	if (argc != 2) {
-		std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
+		std::cout << "Error: Usage: " << argv[0] << " \"<expression>\"" << std::endl;
 		return 1;
 	}
 	try {
 		RPN rpn(argv[1]);
-		std::cout << rpn.getResult() << std::endl;
 	} catch (std::invalid_argument &e) {
-		std::cerr << "Error: " << e.what() << std::endl;
+		std::cout << "Error: " << e.what() << std::endl;
 		return 1;
 	}
 	return 0;
